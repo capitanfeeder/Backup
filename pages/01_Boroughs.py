3ae34258e634ea47f8ee8af12f7c6c3aad2c2a28
Gabriel Sosa <gabrielwesker26@gmail.com>
2024-01-19 03:21:42 -0300

    Create 01_Boroughs.py

---
+++ pages/01_Boroughs.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# Título de la página
+st.title("Boroughs")
+
+# Separador visual
+st.markdown('***')
+
+# Imagen de presentación
+st.image("Datasets/4-Extras/mapaNYC.jpg", use_column_width=True)
+
+
+st.write("""
+**_Población_:** 
+- **Manhattan:** Alrededor de 1.6 millones de personas.
+- **Brooklyn:** Alrededor de 2.6 millones de personas.
+- **Queens:** Alrededor de 2.4 millones de personas.
+- **Bronx:** Alrededor de 1.5 millones de personas.
+- **Staten Island:** Alrededor de 500,000 personas.  
+""")
