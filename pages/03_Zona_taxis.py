6ce2ec82ad9a0fd9111252dcb6a440b238cf3e30
Gabriel Sosa <gabrielwesker26@gmail.com>
2024-01-19 03:21:56 -0300

    Create 03_Zona_taxis.py

---
+++ pages/03_Zona_taxis.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Título de la página
+st.title("Zonas de taxis")
+
+# Separador visual
+st.markdown('***')
+
+# Imagen de presentación
+st.image("Datasets/4-Extras/mapaNYC.jpeg", use_column_width=True)
