2b33dcd79a4d6dc711dff2d80d66fe687e012211
Gabriel Sosa <gabrielwesker26@gmail.com>
2024-01-19 03:22:02 -0300

    Create 04_Heatmap.py

---
+++ pages/04_Heatmap.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Título de la página
+st.title("Zonas de mayor circulación de taxis")
+
+# Separador visual
+st.markdown('***')
+
+# Imagen de presentación
+st.image("Datasets/4-Extras/HeatmapNYC.jpeg", use_column_width=True)
