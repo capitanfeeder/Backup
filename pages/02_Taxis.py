5a5023aa8962daa592b91e1d74bb7ce6ccca5bcd
Gabriel Sosa <gabrielwesker26@gmail.com>
2024-01-19 03:21:50 -0300

    Create 02_Taxis.py

---
+++ pages/02_Taxis.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Título de la página
+st.title("Taxis")
+
+# Separador visual
+st.markdown('***')
+
+# Imagen de presentación
+st.image("Datasets/4-Extras/TipotaxiNYC.jpeg", use_column_width=True)
+
+# Separador visual
+st.markdown('***')
